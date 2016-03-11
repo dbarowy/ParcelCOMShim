c06d616bfbc70bdd84c0e3e149877d3745bb3436
Daniel Barowy <dbarowy@cs.umass.edu>
2016-03-11 12:42:15 -0500

    Add cross-workbook reference type.

--- ParcelCOMShim/ParcelCOMShim.fs
+++ ParcelCOMShim/ParcelCOMShim.fs
@@ -3,17 +3,60 @@
     open Microsoft.Office.Interop.Excel
     open Parcel
 
-    type XLRange = Microsoft.Office.Interop.Excel.Range
+    [<AbstractClass>]
+    type COMRef() =
+        abstract Width : int
+        abstract Height : int
+        abstract Workbook : Workbook
+        abstract Worksheet : Worksheet
+        abstract Range : Range
+        abstract IsFormula : bool
+        abstract Formula : string
+        abstract IsCell : bool
+        abstract Path : string
+        abstract WorkbookName : string
+        abstract WorksheetName : string
+        abstract DoNotPerturb : bool with get, set
 
-    type COMRef(wb: Workbook,
+    type NonLocalComRef(
+                path: string,           // path excluding final separator and filename; option type because in-memory workbooks have no path
+                workbook_name: string,
+                worksheet_name: string,
+                formula: string option) =
+        inherit COMRef()
+        let _path = path
+        let _workbook_name = workbook_name
+        let _worksheet_name = worksheet_name
+        let _formula = formula
+        let mutable _do_not_perturb = match formula with | Some(f) -> true | None -> false
+
+        override self.Width = raise (NotImplementedException())
+        override self.Height = raise (NotImplementedException())
+        override self.Workbook = raise (NotImplementedException())
+        override self.Worksheet = raise (NotImplementedException())
+        override self.Range = raise (NotImplementedException())
+        override self.IsFormula = match _formula with | Some(f) -> true | None -> false
+        override self.Formula = match _formula with
+            | Some(f) -> f
+            | None -> failwith "Not a formula reference."
+        override self.IsCell = raise (NotImplementedException())
+        override self.Path = _path
+        override self.WorkbookName = _workbook_name
+        override self.WorksheetName = _worksheet_name
+        override self.DoNotPerturb
+            with get() = _do_not_perturb
+            and set(value) = _do_not_perturb <- value
+
+    type LocalCOMRef(wb: Workbook,
                 ws: Worksheet,
-                range: XLRange,
+                range: Range,
                 path: string,           // path excluding final separator and filename; option type because in-memory workbooks have no path
                 workbook_name: string,
                 worksheet_name: string,
                 formula: string option,
                 width: int,
                 height: int) =
+        inherit COMRef()
         let _wb = wb
         let _ws = ws
         let _r = range
@@ -26,35 +69,31 @@
         let _formula = formula
         let mutable _do_not_perturb = match formula with | Some(f) -> true | None -> false
 
-        member self.Width = _width
-        member self.Height = _height
-        member self.Workbook = _wb
-        member self.Worksheet = _ws
-        member self.Range = _r
-        member self.IsFormula = match _formula with | Some(f) -> true | None -> false
-        member self.Formula = match _formula with
+        override self.Width = _width
+        override self.Height = _height
+        override self.Workbook = _wb
+        override self.Worksheet = _ws
+        override self.Range = _r
+        override self.IsFormula = match _formula with | Some(f) -> true | None -> false
+        override self.Formula = match _formula with
             | Some(f) -> f
             | None -> failwith "Not a formula reference."
-        member self.IsCell = _is_cell
-        member self.Path = _path
-        member self.WorkbookName = _workbook_name
-        member self.WorksheetName = _worksheet_name
-        member self.DoNotPerturb
+        override self.IsCell = _is_cell
+        override self.Path = _path
+        override self.WorkbookName = _workbook_name
+        override self.WorksheetName = _worksheet_name
+        override self.DoNotPerturb
             with get() = _do_not_perturb
             and set(value) = _do_not_perturb <- value
-        override self.GetHashCode() =
-            let x = range.Column
-            let y = range.Row
-            AST.Address.addressHash x y _worksheet_name
 
     module Address =
-        let GetCOMObject(addr: AST.Address, app: Application) : XLRange =
+        let GetCOMObject(addr: AST.Address, app: Application) : Range =
             let wb: Workbook = app.Workbooks.Item(addr.A1Workbook())
             let ws: Worksheet = wb.Worksheets.Item(addr.A1Worksheet()) :?> Worksheet
-            let cell: XLRange = ws.Range(addr.A1Local())
+            let cell: Range = ws.Range(addr.A1Local())
             cell
 
-        let AddressFromCOMObject(com: Microsoft.Office.Interop.Excel.Range, wb: Microsoft.Office.Interop.Excel.Workbook) : AST.Address =
+        let AddressFromCOMObject(com: Range, wb: Workbook) : AST.Address =
             let wsname = com.Worksheet.Name
             let wbname = wb.Name
             let path = System.IO.Path.GetDirectoryName(wb.FullName)
@@ -63,12 +102,12 @@
 
     module Range =
         // build a contiguous range from topleft and bottomright coordinates
-        let private constructRange(app: Application)(tl: AST.Address)(br: AST.Address) : XLRange =
+        let private constructRange(app: Application)(tl: AST.Address)(br: AST.Address) : Range =
             let wb: Workbook = app.Workbooks.Item(tl.A1Workbook())
             let ws: Worksheet = app.Worksheets.Item(tl.A1Worksheet()) :?> Worksheet
             ws.Range(tl.A1Local(), br.A1Local())
 
         // build a discontiguous range from range components
-        let GetCOMObject(rng: AST.Range, app: Application) : XLRange =
+        let GetCOMObject(rng: AST.Range, app: Application) : Range =
             let rngs = rng.Ranges() |> List.map (fun (tl: AST.Address, br: AST.Address) -> constructRange app tl br)
             List.reduce (fun acc r -> app.Union(acc, r)) rngs
