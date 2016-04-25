4c918315d8c95f570ee24280d821a8f0c59c2695
Daniel Barowy <dbarowy@cs.umass.edu>
2016-04-25 09:47:35 -0400

    initial changes

---
+++ ParcelCOMShim/SpreadsheetShim.fs
@@ -0,0 +1,7 @@
+﻿namespace SpreadsheetShim
+
+    type ICell = class end
+    type IRange = class end
+    type IWorksheet = class end
+    type IWorkbook = class end
+    type ISpreadsheet = class end
